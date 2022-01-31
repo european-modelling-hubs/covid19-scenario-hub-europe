d53aaf488f83d808c65e90f67b9027e6ce0725b3
Hugo Gruson <hugo.gruson@protonmail.com>
2022-01-31 18:08:32 +0100

    Remove scenario_name column

--- template/generate_template.R
+++ template/generate_template.R
@@ -5,12 +5,15 @@ locations_names_eu <- get_hub_config("forecast_locations")
 locations_eu <- read_csv("data-locations/locations_eu.csv")[, c("location_name", "location")]
 target_variables_eu <- get_hub_config("target_variables")
 
-template_us <- read_csv("https://raw.githubusercontent.com/midas-network/covid19-scenario-modeling-hub/master/data-processed/MyTeam-MyModel/2020-12-21-MyTeam-MyModel.csv")
+template_us <- read_csv("https://raw.githubusercontent.com/midas-network/covid19-scenario-modeling-hub/fca8bc2d86553c5bd605341cfe073a048658a6be/data-processed/MyTeam-MyModel/2020-12-21-MyTeam-MyModel.csv")
 
 template_eu <- template_us %>%
   mutate(location = as.factor(location),
          location_name = locations_names_eu[as.numeric(location)],
+         # We only only integers
          value = round(value)) %>%
+  # We remove this column because it's redundant with the scenario_id column
+  select(-scenario_name) %>%
   select(-location) %>%
   left_join(locations_eu) %>%
   select(-location_name) %>%
