c59b0dc4cfd857bf4de7685a4bfaea124f4e0018
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2022-04-04 13:15:29 +0100

    reorganise code

--- code/load/load-from-local.R
+++ code/load/load-from-local.R
@@ -15,46 +15,6 @@ results <- map_dfr(.x = models,
                       mutate(model = .x)) %>%
   mutate(scenario = recode(scenario_id,
                            !!!scenarios$round_0$scenario_labels))
-scenario_caption <- scenarios$round_0$scenario_caption
-
-# plot --------------------------------------------------------------------
-# By scenario
-cross_scenario_deaths <- plot_scenarios(results,
-                                     scenario_caption,
-                                     target_variable = "inc death",
-                                     columns = "scenario")
-ggsave(here("reports", "round-0", "cross-scenario-deaths.png"),
-       height = 35, width = 10)
-
-cross_scenario_cases <- plot_scenarios(results,
-                                     scenario_caption,
-                                     target_variable = "inc case",
-                                     columns = "scenario")
-ggsave(here("reports", "round-0", "cross-scenario-cases.png"),
-       height = 35, width = 10)
-
-# By model
-cross_model_deaths <- plot_scenarios(results,
-                                        scenario_caption,
-                                        target_variable = "inc death",
-                                        columns = "model")
-ggsave(here("reports", "round-0", "cross-model-deaths.png"),
-       height = 35, width = 10)
-
-cross_model_cases <- plot_scenarios(results,
-                                       scenario_caption,
-                                       target_variable = "inc case",
-                                       columns = "model")
-ggsave(here("reports", "round-0", "cross-model-cases.png"),
-       height = 35, width = 10)
-
-
-# Data summaries ----------------------------------------------------------
-
-# How many peaks?
-# Average peak value?
-# Highest peak value?
-# Time from minima to maxima?
 
 
 
---
+++ code/report/plot-scenarios.R
@@ -0,0 +1,50 @@
+# Get results
+library(here)
+library(dplyr)
+library(purrr)
+library(readr)
+
+# get scenario metadata
+source(here("code", "load", "scenarios.R"))
+# get model projections
+source(here("code", "load", "load-from-local.R"))
+# get plotting function
+source(here("code", "load", "plot_scenarios.R"))
+
+# Plot by scenario --------------------------------------------------------
+
+cross_scenario_deaths <- plot_scenarios(results,
+                                        scenarios$round_0$scenario_caption,
+                                     target_variable = "inc death",
+                                     columns = "scenario")
+ggsave(here("reports", "round-0", "cross-scenario-deaths.png"),
+       height = 35, width = 10)
+
+
+cross_scenario_cases <- plot_scenarios(results,
+                                       scenarios$round_0$scenario_caption,
+                                     target_variable = "inc case",
+                                     columns = "scenario")
+ggsave(here("reports", "round-0", "cross-scenario-cases.png"),
+       height = 35, width = 10)
+
+# Plot by model --------------------------------------------------------
+
+cross_model_deaths <- plot_scenarios(results,
+                                     scenarios$round_0$scenario_caption,
+                                        target_variable = "inc death",
+                                        columns = "model")
+ggsave(here("reports", "round-0", "cross-model-deaths.png"),
+       height = 35, width = 10)
+
+cross_model_cases <- plot_scenarios(results,
+                                    scenarios$round_0$scenario_caption,
+                                       target_variable = "inc case",
+                                       columns = "model")
+ggsave(here("reports", "round-0", "cross-model-cases.png"),
+       height = 35, width = 10)
+
+
+
+
+
