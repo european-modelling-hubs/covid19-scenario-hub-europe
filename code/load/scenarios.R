76a5324944e900575358bf3c0c89e2563fce8dde
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2022-06-01 16:38:57 +0100

    Update scenarios.R
    Add round 1

--- code/load/scenarios.R
+++ code/load/scenarios.R
@@ -1,12 +1,22 @@
 # Set scenario labels
 library(tibble)
 scenarios <- list(
+  # Round 0
   "round_0" = list("scenario_labels" = c(
-                     "A-2022-02-25" = "Strong/None",
-                     "B-2022-02-25" = "Strong/New",
-                     "C-2022-02-25" = "Weak/None",
-                     "D-2022-02-25" = "Weak/New"),
+                                       "A-2022-02-25" = "Strong/None",
+                                       "B-2022-02-25" = "Strong/New",
+                                       "C-2022-02-25" = "Weak/None",
+                                       "D-2022-02-25" = "Weak/New"),
                    "origin_date" = as.Date("2022-03-13"),
                    "submission_window_end" = as.Date("2022-04-22"),
-                   "scenario_caption" = "Strong/Weak = stronger/weaker immunity maintained over time; \n None/New = immune evading variant introduced May 2022")
+                   "scenario_caption" = "Strong/Weak = stronger/weaker immunity maintained over time; \n None/New = immune evading variant introduced May 2022"),
+  # Round 1
+  "round_1" = list("scenario_labels" = c(
+                                      "A-2022-05-22" = "Strong/Summer",
+                                      "B-2022-05-22" = "Strong/Autumn",
+                                      "C-2022-05-22" = "Weak/Summer",
+                                      "D-2022-05-22" = "Weak/Autumn"),
+    "origin_date" = as.Date("2022-05-22"),
+    "submission_window_end" = as.Date("2022-06-03"),
+    "scenario_caption" = "Strong/Weak = stronger/weaker immunity maintained over time; \n Summer/Autumn = 60+ second booster campaign starting in summer or autumn")
 )
