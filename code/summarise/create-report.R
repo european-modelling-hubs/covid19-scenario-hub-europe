25a107d71efb1835a07207cf5c3fca75fc98842d
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2022-06-09 01:35:43 +0100

    add r1 report

--- code/summarise/create-report.R
+++ code/summarise/create-report.R
@@ -7,22 +7,18 @@ library(readr)
 source(here("code", "load", "scenarios.R"))
 # get model projections
 source(here("code", "load", "load_local_results.R"))
+# get plots
+source(here("code", "summarise", "create_plots.R"))
 
 # Set round
-round <- 0
-
-# Get results
-scenario_round <- scenarios[[paste0("round_", 0)]]
-results <- load_local_results(round = round)
-
-# Create reporting directory
-report_dir <- paste0(here("reports"), "/round-", round)
-if(!dir.exists(report_dir)){ dir.create(report_dir) }
+round <- 1
 
 # Create plots
-source(here("code", "summarise", "create-plots.R"))
+create_plots(round = round)
 
 # Render report ----------------------------------------------------------
 rmarkdown::render(here("code", "summarise", "results.Rmd"),
-                  output_file = here(report_dir, "report.html"),
+                  output_file = here("reports",
+                                     paste0("round-", round),
+                                     "report.html"),
                   params = list("round" = round))
