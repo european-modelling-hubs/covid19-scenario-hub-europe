e1c64c39ddca81a38b80f6cef011c92465735b7c
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2022-10-23 19:01:17 +0100

    add loading code and data

---
+++ code/load-from-local.R
@@ -0,0 +1,58 @@
+library(readr)
+library(purrr)
+library(here)
+library(dplyr)
+library(lubridate)
+
+# Load results from local repo
+load_local_results <- function(round = NULL, subdir = NULL) {
+
+  if (is.null(subdir)) {subdir <- "data-processed"}
+
+  # load a local copy of scenarios metadata
+  scenarios <- read_rds(here("code", "20221021-scenarios.rds"))
+
+  # get csv paths
+  model_results <- list.files(here(subdir),
+                              full.names = TRUE, recursive = TRUE)
+  model_results <- model_results[grepl(".csv", model_results)]
+
+  # get model date and name
+  models <- tibble(file = model_results) |>
+    mutate(date = as.Date(str_extract(file,
+                                      "2022-[:digit:][:digit:]-[:digit:][:digit:]")),
+           model = str_remove(file, here(subdir)),
+           model = str_extract(model, "\\/(.+)\\/"),
+           model = str_remove_all(model, "\\/"))
+
+  # keep only files within round specific dates
+  origin_date = scenarios[[paste0("round-", round)]][["origin_date"]]
+  window_end = scenarios[[paste0("round-", round)]][["submission_window_end"]]
+  valid_dates <- seq.Date(from = origin_date, to = window_end, by = 1)
+  models <- filter(models, date %in% valid_dates)
+
+  # read csv
+  safely_read <- safely(read_csv, .)
+  results <- map(.x = models$file,
+                 ~ safely_read(.x))
+  names(results) <- models$model
+  results <- transpose(results)
+  results <- bind_rows(results$result, .id = "model") |>
+    mutate(round = round)
+
+  # Standardise values per 100k population
+  pop <- read_csv(here("data-locations", "locations_eu.csv"),
+                  show_col_types = FALSE) %>%
+    select(location, population)
+  results <- left_join(results, pop, by = "location") %>%
+    mutate(value_100k = value / population * 100000) %>%
+    select(-population)
+
+  # add dates
+  results <- results |>
+    mutate(origin_date = origin_date,
+           horizon = as.numeric(str_remove(horizon, " wk")),
+           target_end_date = origin_date + lubridate::weeks(horizon) - 1)
+
+  return(results)
+}
---
+++ code/load.R
@@ -0,0 +1,79 @@
+# Get all submitted csv files, optionally by Round
+library(here)
+library(dplyr)
+library(purrr)
+library(covidHubUtils)
+library(arrow)
+library(curl)
+
+
+load_results <- function(local = FALSE) {
+
+  # Load scenario data --------------------------------------
+
+  if (local) {
+    source(here("code", "load-from-local.R"))
+    results <- load_local_results(round = 1)
+  } else {
+    # get scenario metadata
+    try(source("https://raw.githubusercontent.com/covid19-forecast-hub-europe/covid19-scenario-hub-europe-website/main/code/load/scenarios.R"))
+    # get round 1 path
+    data_path <- tempfile(fileext = ".parquet")
+    url <- "https://github.com/covid19-forecast-hub-europe/covid19-scenario-hub-europe/releases/download/round1/round1.parquet"
+    # download and load into R
+    try(curl::curl_download(url, data_path))
+    results <- try(arrow::read_parquet(data_path))
+  }
+
+  # -------------------------------------------------------------------------
+  # models:
+  # in round 1, USC submitted 2 results from the same model ("USC-SIkJalpha"),
+  # using only new data ~3 weeks apart
+  # keep the later version of the model: "USC-SIkJalpha_update"
+  results <- filter(results,
+                    !grepl("^USC-SIkJalpha$", model))
+
+  # Filter results to countries with multiple models
+  n_model_min <- 2
+  multi_model_targets <- distinct(results, model, location, target_variable) %>%
+    group_by(location, target_variable) %>%
+    tally() %>%
+    filter(n >= n_model_min) |>
+    select(-n)
+  results <- left_join(multi_model_targets, results)
+
+  # Add values per 100k population
+  pop <- read_csv(here("data-locations", "locations_eu.csv"),
+                  show_col_types = FALSE) %>%
+    select(location, population)
+
+  results <- left_join(results, pop, by = "location") %>%
+    mutate(value_100k = value / population * 100000) %>%
+    select(-population)
+
+  # Load truth data ------------------------------------------------------
+  if (local) {
+    # load a local copy (saved 2022-10-21)
+    obs <- read_csv(here("code", "20221021-obs.csv"))
+  } else {
+    obs <- try(load_truth(
+      truth_source = "JHU",
+      temporal_resolution = "weekly",
+      hub = "ECDC"
+    ))
+  }
+
+  obs <- obs |>
+    mutate(obs = value,
+           obs_100k = obs / population * 10000) |>
+    select(-model, -value, -population) |>
+    filter(target_end_date >= min(results$target_end_date))
+
+  # join results & obs -----------------------------------------
+  results <- left_join(results, obs,
+                       by = c("location", "target_variable",
+                              "target_end_date"))
+
+  return(results)
+
+}
