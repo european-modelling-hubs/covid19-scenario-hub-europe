866fdbf57cbc8dcdadb700d1a283357f8bbd7973
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2023-03-27 11:10:06 +0100

    update

--- analysis/code/compare-ensembles.R
+++ analysis/code/compare-ensembles.R
@@ -1,42 +1,75 @@
 # Compare ensembles from samples v from quantiles
-# Compare:
-# a. Number of peaks
-# b. Timing of peaks
-# c. Duration of waves
-# d. Cumulative burden
+# Cumulative totals; peaks; average differences in value at each quantile
+#
 
-# peak <- ensembles |>
-#   group_by(quantile, location, target_variable, scenario_id, model) |>
-#   filter(value == max(value))
+# # Compare cumulative -----------------------------------------------------
+# cumulative_proj <- results |>
+#   group_by(location, target_variable, scenario_id,
+#            model, sample) |>
+#   arrange(target_end_date) |>
+#   mutate(proj_cumulative = cumsum(value)) |>
+#   filter(target_end_date == max(target_end_date)) |>
+#   ungroup() |>
+#   mutate(proj_cumulative_p = proj_cumulative / population * 100)
+#
+# cumulative_past <- read_csv(here("analysis", "data", "obs.csv")) |>
+#   filter(target_end_date >= min(results$target_end_date) - lubridate::weeks(52) &
+#            target_end_date < min(results$target_end_date) &
+#            location %in% unique(results$location) &
+#            target_variable %in% unique(results$target_variable)) |>
+#   group_by(location, target_variable) |>
+#   arrange(target_end_date) |>
+#   mutate(past_cumulative = cumsum(value)) |>
+#   filter(target_end_date == max(target_end_date)) |>
+#   ungroup() |>
+#   mutate(past_cumulative_p = past_cumulative / population * 100) |>
+#   select(location, target_variable, past_cumulative_p)
+#
+# cumulative <- left_join(cumulative_proj, cumulative_past)
+# cumulative_exceeding <- cumulative |>
+#   group_by(location, target_variable) |>
+#   summarise(exceed = sum(proj_cumulative_p > past_cumulative_p),
+#             n = n(),
+#             exceed_p = exceed / n() * 100)
 
-# Compare average differences at each quantile level -------------------------
 
-diffs <- ensembles |>
-  select(-n) |>
-  group_by(quantile, location, target_variable, scenario_id) |>
-  pivot_wider(names_from = model) |>
-  summarise(diff_sum = sum(Samples - Quantiles),
-            diff_med = median(Samples - Quantiles),
-            .groups = "drop")
+# # Compare peaks -----------------------------------------------------------
+# span <- 3
+# peaks <- results |>
+#   mutate(sample_id = paste(location, target_variable, scenario_id, model, sample,
+#                            sep = "-")) |>
+#   group_by(sample_id) |>
+#   mutate(target_variable = as.factor(target_variable),
+#          peak_val = splus2R::peaks(x = value, span = span),
+#          peak_obs = splus2R::peaks(x = obs, span = span),
+#          peak_t_pos = peak_val & peak_obs,
+#          peak_t_neg = !peak_val & !peak_obs,
+#          peak_f_pos = peak_val & !peak_obs,
+#          peak_f_neg = !peak_val & peak_obs) |>
+#   filter(!is.na(obs)) # 28 weeks of data
+#
+# peak_obs <- peaks |>
+#   summarise(n = n(),
+#             true_pos = sum(peak_t_pos),
+#             true_neg = sum(peak_t_neg),
+#             false_pos = sum(peak_f_pos),
+#             false_neg = sum(peak_f_neg),
+#             sty = true_pos / (true_pos + false_neg),
+#             spy = true_neg / (false_pos + true_neg))
+#
+# plot_obs <- peaks |>
+#   ungroup() |>
+#   select(target_end_date, location, target_variable, obs, peak_obs) |>
+#   distinct() |>
+#   mutate(target = paste(location, target_variable))
+#
+# plot_obs <- plot_obs |>
+#   ggplot(aes(x = target_end_date, y = obs, col = target)) +
+#   geom_line() +
+#   geom_point(aes(alpha = peak_obs)) +
+#   facet_wrap(~ target, scales = "free_y")
 
-diffs_plot <- diffs |>
-  mutate(quantile = forcats::fct_inseq(gsub("q0", "", quantile))) |>
-  filter(!scenario_id == "Weighted") |>
-  ggplot(aes(x = scenario_id, y = diff_med)) +
-  geom_point(aes(col = quantile)) +
-  scale_colour_viridis_d(option = "D") +
-  geom_hline(aes(yintercept = 0), lty = 3, alpha = 0.5) +
-  labs(y = "Median difference per 100k",
-       x = "Scenario",
-       fill = "Quantile",
-       subtitle = "Differences between ensembles from samples or quantiles, median difference over time",
-       caption = "Median difference: 52 week median of (sample ensemble - quantile ensemble) at each quantile") +
-  facet_wrap(~location + target_variable,
-             scales = "free_y") +
-  theme_bw()
-
-# ---------------------------------
-# width plot
+# # Compare average differences at each quantile level -------------------------
 
 interval_ensembles <- ensembles |>
   mutate(quantile = as.numeric(as.character(sub("q0", "", quantile))),
--- analysis/code/plot.R
+++ analysis/code/plot.R
@@ -1,88 +1,109 @@
 # Plot ensemble results with samples
+#
+## Create ensemble with:
+# source(here("analysis", "code", "create-ensembles.R"))
+# ensembles <- create_ensembles(results = results,
+#                               truncate_weeks = 2,
+#                               quantiles = c(0.02, 0.1, 0.5, 0.90, 0.98))
 library(dplyr)
 library(ggplot2)
 
 plot_ensemble_results <- function(ensembles, results,
-                                  set_target_variable = "inc case",
-                                  set_location = "NL") {
+                                  set_target) {
 
+  latest_data <- max(subset(results, !is.na(obs))$target_end_date)
   # Reshape data for plotting -----
   # ensembles
   ensembles <- ensembles |>
+    mutate(model = ifelse(scenario_id == "Weighted",
+                          "Weighted samples", model),
+           value = ifelse(scenario_id == "Weighted" &
+                            target_end_date < latest_data, NA, value)) |>
     pivot_wider(names_from = quantile) |>
     mutate(median = q0.5) |>
     select(-q0.5)
 
   # samples
   samples <- results |>
-    mutate(model_sample = paste0(scenario_id, "_", model, "_", sample),
+    mutate(model_sample = paste(location, target_variable, scenario_id,
+                                model, sample,
+                                sep = "-"),
+           # relabel model to None
            model = "None") |>
     rename(q0.5 = value_100k) |>
-    select(location, target_variable, target_end_date, scenario_id,
+    select(location, target_variable,
+           target_end_date, scenario_id,
            model, model_sample, q0.5)
 
-  samples_wtd <- samples |>
-    mutate(scenario_id = "Weighted")
-
-  # combine data and observations
-  plot_data <- bind_rows(ensembles, samples, samples_wtd) |>
+    # combine data and observations
+  plot_data <- bind_rows(ensembles, samples) |>
     left_join(distinct(results,
                        location, target_variable,
                        target_end_date, obs_100k),
               by = c("location", "target_variable", "target_end_date"))
 
-  # Format for plotting
+  # Format for plotting -----------------------------------------------------
   plot_data <- plot_data |>
     # set order for facet rows
-    mutate(Ensemble = ordered(model,
+    mutate(model = ordered(model,
                            levels = c("None",
+                                      "Quantiles",
                                       "Samples",
-                                      "Quantiles"))) |>
-    # get proper location names
-    left_join(results |>
-                distinct(location, location_name),
-              by = "location") |>
-    # keep only one target
-    filter(location == set_location &
-             target_variable == set_target_variable)
+                                      "Weighted samples"),
+                           labels = c("A. All samples",
+                                      "B. Quantile",
+                                      "C. Simulations",
+                                      "D. Weighted")),
+           obs_100k = ifelse(model %in% c("Quantiles", "Samples"), NA, obs_100k))
 
-  # Plot -----
+  # colours for scenarios
+  scenario_colours <- c("A" = "#e66101",
+                        "B" = "#fdb863",
+                        "C" = "#5e3c99",
+                        "D" = "#b2abd2",
+                        "Weighted" = "grey50")
+
+  # Plot --------------------------------------------------------------
   plot <- plot_data |>
-    ggplot(aes(x = target_end_date)) +
+    mutate(target = paste(location, target_variable)) |>
+    filter(target == set_target) |>
+    ggplot(aes(x = target_end_date,
+               fill = scenario_id, col = scenario_id)) +
     # ----- Geoms
     # ensembles
-    geom_ribbon(aes(ymin = q0.01, ymax = q0.99,
-                    fill = Ensemble), alpha = 0.2) +
-    geom_ribbon(aes(ymin = q0.05, ymax = q0.95,
-                    fill = Ensemble), alpha = 0.4) +
-    geom_ribbon(aes(ymin = q0.25, ymax = q0.75,
-                    fill = Ensemble), alpha = 0.6) +
-    geom_line(aes(y = median,
-                  col = Ensemble), size = 1) +
+    geom_ribbon(aes(ymin = q0.01, ymax = q0.99), alpha = 0.2, col = NA) +
+    geom_ribbon(aes(ymin = q0.25, ymax = q0.75), alpha = 0.6, col = NA) +
+    geom_line(aes(y = median), size = 1) +
     # model samples
-    geom_line(aes(y = q0.5,
-                  group = model_sample, col = Ensemble), alpha = 0.1) +
+    geom_line(aes(y = q0.5, group = model_sample), alpha = 0.1) +
     # observed data as points
-    geom_point(aes(y = obs_100k), size = 0.7) +
+    geom_point(aes(y = obs_100k), colour = "black", size = 0.5,
+               show.legend = FALSE) +
     # ----- Structure
     # facets
-    facet_grid(rows = vars(Ensemble),
-               cols = vars(scenario_id),
+    facet_wrap(~ model,
+               ncol = 1,
                scales = "fixed") +
     # labels
-    labs(x = NULL,
-         y = paste0("Incidence per 100,000"), x = "2022-2023",
-         subtitle = paste0(unique(plot_data$location_name),
-                           " ", gsub("inc ", "", set_target_variable), " ",
-                           "multi-model projections, 2022-23"),
-         caption = "Showing median, 75%, 95%, and 99% confidence") +
-    # colours
-    scale_colour_brewer(type = "qual", palette = "Dark2",
+    labs(x = NULL, y = NULL,
+         colour = "Scenario", fill = "Scenario",
+         caption = set_target) +
+    # colours and scales
+    scale_colour_manual(values = scenario_colours,
                         aesthetics = c("colour", "fill")) +
     scale_x_date(date_breaks = "3 month", date_labels = "%b") +
     # theme
     theme_bw() +
-    theme(legend.position = "bottom")
+    theme(legend.position = "bottom",
+          axis.line = element_line(linewidth = 0.25),
+          strip.background = element_blank(),
+          #panel.spacing = unit(0, "lines"),
+          panel.border = element_blank(),
+          panel.grid = element_blank())
+  if (set_target != "ES inc case") {
+    plot <- plot +
+      theme(strip.text = element_text(colour = "white"))
+  }
 
   return(plot)
 }
